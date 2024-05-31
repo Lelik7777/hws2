4d7d6fd5e1b13c1dea0bf8951748ea1eed59ac22
Lelik Jan <21alex7777@gmail.com>
2024-05-31 16:52:46 +0300

    feat: implement hw11

--- src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
+++ src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
@@ -1,15 +1,42 @@
-import React from 'react'
-import {Slider, SliderProps} from '@mui/material'
+import React from "react";
+import { Slider, SliderProps } from "@mui/material";
 
-const SuperRange: React.FC<SliderProps> = (props) => {
-    return (
-        <Slider
-            sx={{ // стили для слайдера // пишет студент
-                
-            }}
-            {...props} // отдаём слайдеру пропсы если они есть (value например там внутри)
-        />
-    )
-}
+const SuperRange: React.FC<SliderProps> = props => {
+  return (
+    <Slider
+      sx={{
+        color: "#00CC22",
+        "& .MuiSlider-track": {
+          color: "#8B8B8B", // цвет неактивной части слайдера
+        },
+        "& .MuiSlider-thumb": {
+          width: 18, // Ширина бегунка
+          height: 18, // Высота бегунка
+          backgroundColor: "white", // Цвет фона бегунка (зеленый)
+          border: 1,
+          borderColor: "#00CC22", // Без границы
+          borderRadius: "50%", // Круглая форма бегунка
 
-export default SuperRange
+          "&:before": {
+            content: '""',
+            position: "absolute",
+            width: 6, // Ширина точки внутри бегунка
+            height: 6, // Высота точки внутри бегунка
+            backgroundColor: "#00CC22", // Цвет точки (зеленый)
+            borderRadius: "50%", // Круглая форма точки
+            top: "50%", // Центрирование точки по вертикали
+            left: "50%", // Центрирование точки по горизонтали
+            transform: "translate(-50%, -50%)", // Центрирование точки
+          },
+          "&:focus, &:hover": {
+            borderColor: "#8B8B8B", // Тень при фокусе/наведении/активации
+          },
+        },
+        // стили для слайдера // пишет студент
+      }}
+      {...props} // отдаём слайдеру пропсы если они есть (value например там внутри)
+    />
+  );
+};
+
+export default SuperRange;
